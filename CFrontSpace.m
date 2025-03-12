fc4973d0eb81cdb640b5c8e01b64fbd710e289c9
Alexander Tumanov <tumanov239@gmail.com>
2025-03-12 13:47:46 +0100

    Update CFrontSpace.m

--- CFrontSpace.m
+++ CFrontSpace.m
@@ -1,11 +1,11 @@
 (* ::Package:: *)
 
 Block[{$Path = DirectoryName[$InputFileName]},
-	<<SymbolExpansion`
+	<<SymbExpand`
 ]
 
 version=1.0;
-Print["C front space ",ToString@NumberForm[version,{20,1}]]
+Print["CFrontSpace ",ToString@NumberForm[version,{20,1}]]
 Print["by Alexander G Tumanov"]
 
 Clear[UtoL,LtoU,LtoO,UtoO]
